4016cf624ff43d9e06f2434a2a97d5e6f9f1fa42
Pooya Nazari <npooya12@gmail.com>
2021-01-17 00:23:11 +0330

    adding pdf Viwer Activity

---
+++ app/src/main/java/com/example/topdfer/PDFViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.topdfer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PDFViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_d_f_view);
+    }
+}
